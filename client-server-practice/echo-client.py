a23c5111b829a1997443b0a812cd118949429652
John Francis <john9francis@gmail.com>
2023-06-15 10:24:58 -0400

    trying a different IP

--- client-server-practice/echo-client.py
+++ client-server-practice/echo-client.py
@@ -3,7 +3,7 @@
 
 import socket
 
-HOST = "2603:6080:8200:9922:e814:7730:5b5e:146e"  # The server's hostname or IP address
+HOST = "fe80::4eb6:5625:7eed:dab%21"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
