569ae603de4b175c25f5a4fa26de41ec6e5f388f
John Francis <john9francis@gmail.com>
2023-06-16 15:19:01 -0400

    added a way for the client to quit

--- client-server-practice/echo-server.py
+++ client-server-practice/echo-server.py
@@ -18,24 +18,26 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     conn, addr = s.accept()
     with conn:
         print(f"Connected by {addr}")
+        while True:
+            welcome_message = f"Would you like me to... \n1.{request1}, \n2.{request2}, \n3.{request3}, or \n4. Quit? " + '\n(Please enter "1", "2", or "3".)'
 
-        welcome_message = f"Would you like me to... \n1.{request1}, \n2.{request2}, or \n3.{request3}? " + '\n(Please enter "1", "2", or "3".)'
+            conn.sendall(welcome_message.encode())
 
-        conn.sendall(welcome_message.encode())
+            data = conn.recv(1024)
 
-        data = conn.recv(1024)
+            conn.sendall(b'You chose: ' + data + b'.')
 
-        conn.sendall(b'You chose: ' + data + b'.')
-
-        if data.decode() == "1":
-            # do response1
-            pass
-        if data.decode() == "2":
-            # do response2
-            pass
-        if data.decode() == "3":
-            # do response3
-            pass
-        else: 
-            # throw an error
-            pass
+            if data.decode() == "1":
+                # do response1
+                pass
+            if data.decode() == "2":
+                # do response2
+                pass
+            if data.decode() == "3":
+                # do response3
+                pass
+            if data.decode() == "4":
+                break
+            else: 
+                # throw an error
+                pass
