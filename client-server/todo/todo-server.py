8b9611dba37bc43a302ccf1dee08dafff660b8b1
John Francis <john9francis@gmail.com>
2023-06-16 17:58:42 -0400

    setting up some read and write functions, haven't implimented them yet

--- client-server/todo/todo-server.py
+++ client-server/todo/todo-server.py
@@ -5,6 +5,22 @@ import socket
 HOST = "0.0.0.0"  # binding to all network interfaces
 PORT = 60000  # Port to listen on (non-privileged ports are > 1023)
 
+file = "todo_list.txt"
+
+# functions we will be using to modify the todo list
+def write_todo(todo,filename):
+    '''Takes in a todo string and a file and writes the todo to the file on a new line.'''
+    with open(filename, "w") as f:
+        f.write(todo)
+        f.write('\n')
+
+def read_file(filename):
+    '''Takes in a filename and returns the contents of the file as a string'''
+    with open(filename, "r") as f:
+        return filename.read()
+
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     print("Waiting for a connection...")
@@ -13,14 +29,14 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     with conn:
         print(f"Connected by {addr}")
 
-        action1 = "view to-do list"
-        action2 = "add a to-do"
-        action3 = "mark a to-do as complete"
+        view = "view to-do list"
+        add = "add a to-do"
+        complete = "mark a to-do as complete"
 
         welcome_message = f'Welcome to the to-do list.' + \
-                          f'\nEnter "1" to {action1}, ' + \
-                          f'\n"2" to {action2}, ' + \
-                          f'\n"3" to {action3}, ' + \
+                          f'\nEnter "1" to {view}, ' + \
+                          f'\n"2" to {add}, ' + \
+                          f'\n"3" to {complete}, ' + \
                           f'\nor "Quit" to quit.'
         conn.sendall(welcome_message.encode())
 
@@ -30,6 +46,16 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             if data.decode() == "quit" or data.decode() == "Quit":
                 conn.sendall(b"Quitting...")
                 break
+            if data.decode() == '1':
+                # view to-do list
+                pass
+            if data.decode() == '2':
+                # add a to-do
+                pass
+            if data.decode() == '3':
+                # mark a to-do as complete
+                pass
+
             else:
-                conn.sendall(b'You said: ' + data + b'.')
+                conn.sendall(b'Sorry, That\'s not an option. Please try again.')
 
