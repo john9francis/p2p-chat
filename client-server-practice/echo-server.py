d2cceac938a95b22277f811d2f1516b84ca75534
John Francis <john9francis@gmail.com>
2023-06-16 15:52:19 -0400

    modified program to just echo.

--- client-server-practice/echo-server.py
+++ client-server-practice/echo-server.py
@@ -7,9 +7,6 @@ HOST = "0.0.0.0"  # Trying to bind to all network interfaces
 PORT = 60000  # Port to listen on (non-privileged ports are > 1023)
 
 # three requests that the server can respond to
-request1 = "Echo"
-request2 = "Tell a joke"
-request3 = "Access a database"
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
@@ -19,28 +16,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     with conn:
         print(f"Connected by {addr}")
 
-        welcome_message = f"Would you like me to... \n1.{request1}, \n2.{request2}, \n3.{request3}, or \n4. Quit? " + '\n(Please enter "1", "2", or "3".)'
-
+        welcome_message = "Welcome to the Echo server. Whatever you type, I will echo."
         conn.sendall(welcome_message.encode())
 
         while True:
             data = conn.recv(1024)
-            if data.decode() == "1":
-                # do response1
-                conn.sendall(b'You chose: ' + data + b'.')
+            conn.sendall(b'You said: ' + data + b'.')
 
-            if data.decode() == "2":
-                # do response2
-                conn.sendall(b'You chose: ' + data + b'.')
-
-            if data.decode() == "3":
-                # do response3
-                conn.sendall(b'You chose: ' + data + b'.')
-
-            if data.decode() == "4":
+            if data.decode() == "quit" or data.decode() == "Quit":
                 conn.sendall(b"Quitting...")
                 break
-            
-            else: 
-                # throw an error
-                pass
